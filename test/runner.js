2f436873a4b8a9118fcc153790d0a62a750e6f52
Benjamin Sattler <bsattler.inbox@gmail.com>
2020-05-18 22:24:37 +0200

    Replace puppeteer with playwright

--- test/runner.js
+++ test/runner.js
@@ -1,18 +1,20 @@
 #!/usr/bin/env node
 
-const puppeteer = require('puppeteer');
+const playwright = require('playwright');
 const connect = require('connect');
 const serveStatic = require('serve-static');
 const http = require('http');
 
+const product = process.env.BROWSER || 'chromium';
 const port = process.argv[2] || 8000;
 const app = connect();
 app.use(serveStatic(`${__dirname}/../`, { index: ['index.html'] }));
 const server = http.createServer(app).listen(port);
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+  const browser = await playwright[product].launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
   let exitCode = 0;
   page.on('console', async (msg) => {
     const args = await Promise.all(msg.args().map((arg) => arg.jsonValue()));
