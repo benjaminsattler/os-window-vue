98a7e7034d21dd15df7ed075012927f3138db5d0
Benjamin Sattler <bsattler.inbox@gmail.com>
2020-06-01 19:30:41 +0200

    Register as a plugin

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 /* eslint-disable import/extensions */
 import OsWindowVue from './lib/OsWindowVue.js';
 
+export function install(Vue) {
+  Vue.component('os-window-vue', OsWindowVue);
+  Vue.config.ignoredElements.push('os-window');
+}
+
 export default OsWindowVue;
